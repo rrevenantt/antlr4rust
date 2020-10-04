09ce192bda98d8a44e91238fd88c9262978ff3ba
Dylan Frankland <dfrankland@users.noreply.github.com>
2020-10-04 12:47:30 -0700

    Only build test grammars when using a feature flag and use env var for antlr4 path

--- build.rs
+++ build.rs
@@ -1,62 +1,76 @@
-use std::convert::TryInto;
-use std::env;
-use std::env::VarError;
-use std::error::Error;
-use std::fs::{read_dir, DirEntry, File};
-use std::io::Write;
-use std::path::Path;
-use std::process::Command;
-
 fn main() {
-    let grammars = vec![
-        "CSV",
-        "ReferenceToATN",
-        "XMLLexer",
-        "SimpleLR",
-        "Labels",
-        "FHIRPath",
-    ];
-    let additional_args = vec![Some("-visitor"), None, None, None, None];
-    let antlr_path = "/home/rrevenantt/dev/antlr4/tool/target/antlr4-4.8-2-SNAPSHOT-complete.jar";
-
-    for (grammar, arg) in grammars.into_iter().zip(additional_args) {
-        //ignoring error because we do not need to run anything when deploying to crates.io
-        let _ = gen_for_grammar(grammar, antlr_path, arg);
+    #[cfg(feature = "generate_test_grammars")]
+    {
+        self::antlr_tool::generate_grammars();
     }
-
-    println!("cargo:rerun-if-changed=build.rs");
-
-    println!("cargo:rerun-if-changed=/home/rrevenantt/dev/antlr4/tool/target/antlr4-4.8-2-SNAPSHOT-complete.jar");
 }
 
-fn gen_for_grammar(
-    grammar_file_name: &str,
-    antlr_path: &str,
-    additional_arg: Option<&str>,
-) -> Result<(), Box<Error>> {
-    // let out_dir = env::var("OUT_DIR").unwrap();
-    // let dest_path = Path::new(&out_dir);
+#[cfg(feature = "generate_test_grammars")]
+mod antlr_tool {
+    use std::{env, process::Command};
 
-    let input = env::current_dir().unwrap().join("grammars");
-    let file_name = grammar_file_name.to_owned() + ".g4";
+    const ANTLR_PATH: &str = "ANTLR_PATH";
 
-    let c = Command::new("java")
-        .current_dir(input)
-        .arg("-cp")
-        .arg(antlr_path)
-        .arg("org.antlr.v4.Tool")
-        .arg("-Dlanguage=Rust")
-        .arg("-o")
-        .arg("../tests/gen")
-        .arg(&file_name)
-        .args(additional_arg)
-        .spawn()
-        .expect("antlr tool failed to start")
-        .wait_with_output()?;
-    // .unwrap()
-    // .stdout;
-    // eprintln!("xx{}",String::from_utf8(x).unwrap());
+    const GRAMMARS_AND_ADDITION_ARGS: &[(&str, Option<&str>)] = &[
+        ("CSV", Some("-visitor")),
+        ("Labels", None),
+        ("Perf", None),
+        ("ReferenceToATN", None),
+        ("SimpleLR", None),
+        ("XMLLexer", None),
+    ];
 
-    println!("cargo:rerun-if-changed=grammars/{}", file_name);
-    Ok(())
+    fn antlr_path_env() -> String {
+        env::var(ANTLR_PATH).expect("Missing ANTLR_PATH environmental variable")
+    }
+
+    pub fn generate_grammars() {
+        for (grammar, arg) in GRAMMARS_AND_ADDITION_ARGS {
+            gen_for_grammar(grammar, *arg);
+        }
+
+        println!("cargo:rerun-if-changed=build.rs");
+
+        println!("cargo:rerun-if-env-changed={}", ANTLR_PATH);
+
+        println!("cargo:rerun-if-changed={}", antlr_path_env());
+    }
+
+    fn gen_for_grammar(grammar_file_name: &str, additional_arg: Option<&str>) {
+        let input = env::current_dir()
+            .expect("Could not retrieve current directory")
+            .join("grammars");
+        let file_name = grammar_file_name.to_owned() + ".g4";
+
+        let exit_status = Command::new("java")
+            .current_dir(input)
+            .arg("-cp")
+            .arg(antlr_path_env())
+            .arg("org.antlr.v4.Tool")
+            .arg("-Dlanguage=Rust")
+            .arg("-o")
+            .arg("../tests/gen")
+            .arg(&file_name)
+            .args(additional_arg)
+            .spawn()
+            .expect("antlr tool failed to start")
+            .wait()
+            .expect("antlr tool wasn't running");
+
+        assert!(
+            exit_status.success(),
+            "antlr tool failed to generate grammar"
+        );
+
+        let exit_status = Command::new("cargo")
+            .arg("fmt")
+            .spawn()
+            .expect("`cargo fmt` failed")
+            .wait()
+            .expect("rustfmt wasn't running");
+
+        assert!(exit_status.success(), "rustfmt failed to format");
+
+        println!("cargo:rerun-if-changed=grammars/{}", file_name);
+    }
 }
