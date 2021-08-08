680af1b7af95b08d9dc32b805f71a6384079b6f4
Konstantin Anisimov <rrevenantt@gmail.com>
2021-08-08 23:57:12 +0300

    support for visitors that return data

--- tests/gen/csvvisitor.rs
+++ tests/gen/csvvisitor.rs
@@ -1,7 +1,8 @@
 #![allow(nonstandard_style)]
 // Generated from CSV.g4 by ANTLR 4.8
 use super::csvparser::*;
-use antlr_rust::tree::ParseTreeVisitor;
+use antlr_rust::tree::{ParseTreeVisitor, ParseTreeVisitorCompat};
+use std::mem;
 
 /**
  * This interface defines a complete generic visitor for a parse tree produced
@@ -31,4 +32,61 @@ pub trait CSVVisitor<'input>: ParseTreeVisitor<'input, CSVParserContextType> {
      * @param ctx the parse tree
      */
     fn visit_field(&mut self, ctx: &FieldContext<'input>) { self.visit_children(ctx) }
+}
+
+pub trait CSVVisitorCompat<'input>:
+    ParseTreeVisitorCompat<'input, Node = CSVParserContextType>
+{
+    /**
+     * Visit a parse tree produced by {@link CSVParser#csvFile}.
+     * @param ctx the parse tree
+     */
+    fn visit_csvFile(&mut self, ctx: &CsvFileContext<'input>) -> Self::Return {
+        self.visit_children(ctx)
+    }
+
+    /**
+     * Visit a parse tree produced by {@link CSVParser#hdr}.
+     * @param ctx the parse tree
+     */
+    fn visit_hdr(&mut self, ctx: &HdrContext<'input>) -> Self::Return { self.visit_children(ctx) }
+
+    /**
+     * Visit a parse tree produced by {@link CSVParser#row}.
+     * @param ctx the parse tree
+     */
+    fn visit_row(&mut self, ctx: &RowContext<'input>) -> Self::Return { self.visit_children(ctx) }
+
+    /**
+     * Visit a parse tree produced by {@link CSVParser#field}.
+     * @param ctx the parse tree
+     */
+    fn visit_field(&mut self, ctx: &FieldContext<'input>) -> Self::Return {
+        self.visit_children(ctx)
+    }
+}
+
+impl<'input, T> CSVVisitor<'input> for T
+where
+    T: CSVVisitorCompat<'input>,
+{
+    fn visit_csvFile(&mut self, ctx: &CsvFileContext<'input>) {
+        let result = <Self as CSVVisitorCompat>::visit_csvFile(self, ctx);
+        *<Self as ParseTreeVisitorCompat>::temp_result(self) = result;
+    }
+
+    fn visit_hdr(&mut self, ctx: &HdrContext<'input>) {
+        let result = <Self as CSVVisitorCompat>::visit_hdr(self, ctx);
+        *<Self as ParseTreeVisitorCompat>::temp_result(self) = result;
+    }
+
+    fn visit_row(&mut self, ctx: &RowContext<'input>) {
+        let result = <Self as CSVVisitorCompat>::visit_row(self, ctx);
+        *<Self as ParseTreeVisitorCompat>::temp_result(self) = result;
+    }
+
+    fn visit_field(&mut self, ctx: &FieldContext<'input>) {
+        let result = <Self as CSVVisitorCompat>::visit_field(self, ctx);
+        *<Self as ParseTreeVisitorCompat>::temp_result(self) = result;
+    }
 }
