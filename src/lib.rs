aa487c7d7ea87ee6b8441f0a931ef209147ad0e9
Zhaoping Lu <zhaopinglu77@gmail.com>
2021-12-07 16:13:59 +0800

    Remove feature raw

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![crate_type = "lib"]
 #![feature(try_blocks)]
 //#![feature(nll)]
-#![feature(raw)]
+// #![feature(raw)]
 #![feature(is_sorted)]
 #![feature(cell_update)]
 #![feature(get_mut_unchecked)]
